6800e2d41f062bc216e32ae8a0673cd24de13940
Claire Lesh <cteterita@gmail.com>
2020-08-29 14:06:55 -0700

    refactor out Total component

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
 import { USCurrencyFormat } from '../App'; //TODO: Should this be passed as a prop?
 import Summary from '../Summary/Summary';
+import Total from '../Total/Total';
 
 class Cart extends Component {
     render() {
-        // TODO: Make summary component
-        
-
-        //TODO: Make total component
-        const total = Object.keys(this.props.selected).reduce(
-            (acc, curr) => acc + this.props.selected[curr].cost,
-            0
+        return (
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                <Summary selected={this.props.selected} />
+                <Total selected={this.props.selected} />
+            </section>
         );
-
-        return (<section className="main__summary">
-        <h2>Your cart</h2>
-        <Summary 
-            selected={this.props.selected}
-        />
-        <div className="summary__total">
-          <div className="summary__total__label">Total</div>
-          <div className="summary__total__value">
-            {USCurrencyFormat.format(total)}
-          </div>
-        </div>
-      </section>)
     }
 }
 
