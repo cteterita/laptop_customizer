3948f4e0527eac6eb07e673d551bececf2343f70
Claire Lesh <cteterita@gmail.com>
2020-08-29 15:01:12 -0700

    refactored out Feature component

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,43 +1,19 @@
 import React, { Component } from 'react';
 
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-import { USCurrencyFormat } from '../App';
+import Feature from '../Feature/Feature';
 
 class Features extends Component {
     render() {
-        //TODO: create feature component
         const features = Object.keys(this.props.features).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            //TODO: Create options component
-            const options = this.props.features[feature].map(item => {
-              const itemHash = slugify(JSON.stringify(item));
-              return (
-                <div key={itemHash} className="feature__item">
-                  <input
-                    type="radio"
-                    id={itemHash}
-                    className="feature__option"
-                    name={slugify(feature)}
-                    checked={item.name === this.props.selected[feature].name}
-                    onChange={e => this.props.updateFeature(feature, item)}
-                  />
-                  <label htmlFor={itemHash} className="feature__label">
-                    {item.name} ({USCurrencyFormat.format(item.cost)})
-                  </label>
-                </div>
-              );
-            });
-      
             return (
-              <fieldset className="feature" key={featureHash}>
-                <legend className="feature__name">
-                  <h3>{feature}</h3>
-                </legend>
-                {options}
-              </fieldset>
+                <Feature 
+                    selected={this.props.selected}
+                    features={this.props.features}
+                    updateFeature={this.props.updateFeature}
+                    feature={feature}
+                    idx={idx}
+                    key={idx}
+                />
             );
         });
 
